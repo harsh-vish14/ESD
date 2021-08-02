347069ae63e7b735ebf4fb4b4937dba8a52f44a4
harsh-vish14 <harshkv9322@gmail.com>
2021-08-02 21:59:23 +0530

    mobile get and dynamic data with name get, post 👍

--- app.js
+++ app.js
@@ -1,27 +1,58 @@
 const dotenv = require("dotenv");
 dotenv.config();
-
 require("./database/connection");
+const Mobile = require("./database/models/mobile");
+
 const express = require("express");
-
 const app = express();
-
 const PORT = process.env.PORT || 8000;
 app.use(express.json());
 
 // this is get request method
-app.post("/mobile", async (req, res) => {
+app.get("/mobile", async (req, res) => {
   //all mobiles will be display here with name, brand, price, origin
+  const mobileData = await Mobile.find({}, { _id: 0 }).catch((err) => {
+    res.status(404).json({ message: err.message });
+  });
+  res.status(200).json(mobileData);
 });
 
 app
   .route("/mobile/:name")
-  .get((req, res) => {
+  .get(async (req, res) => {
     // the information of mobile with name
+
+    const name = req.params.name.split("-").join(" ");
+    const mobileData = await Mobile.find({ mobileName: name }, { _id: 0 });
+    res.status(200).json(mobileData);
   })
   .post(async (req, res) => {
-    // only authenticated user can use this method
-    // adding information of mobile with name
+    /* only authenticated user can use this method
+    adding information of mobile with name */
+
+    if (req.headers.authorization === process.env.AUTH_ID) {
+      const name = req.params.name.split("-").join(" ");
+
+      if (!req.body || !req.body.brand || !req.body.price || !req.body.origin) {
+        res.status(204).json({ message: "invalid response" });
+      }
+
+      const mobileByName = await Mobile.find({ mobileName: name }, { _id: 0 });
+      if (!mobileByName.length) {
+        const mobile = new Mobile({
+          mobileName: name,
+          brand: req.body.brand,
+          price: req.body.price,
+          origin: req.body.origin,
+        });
+        mobile.save();
+        res.status(200).send({ message: "data added successfully" });
+      } else {
+        res.status(200).send({ message: "duplicate data found" });
+      }
+    } else {
+      res.status(401).json({ error: "Invalid authorization" });
+    }
   })
   .put((req, res) => {
     // only authenticated user can use this method
@@ -31,6 +62,10 @@ app
     // only authenticated user can use this method
     // deleting information of mobile with name
   });
+
+app.use(function (req, res, next) {
+  res.status(404).json({ error: "Sorry can't find that!" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`);
--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ dotenv.config();
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_DB_URL, {
+  .connect(process.env.MONGO_DB_URL_DEVELOPMENT, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
