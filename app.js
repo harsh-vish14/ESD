653aea9827ac7d6882d9dec04b76fe5564bc3ada
harsh-vish14 <harshkv9322@gmail.com>
2021-08-03 22:52:35 +0530

    removed bugs and updated model

--- app.js
+++ app.js
@@ -12,9 +12,9 @@ app.get("/mobile", async (req, res) => {
   //all mobiles will be display here with name, brand, price, origin
   const mobileData = await Mobile.find(
     {},
-    { _id: 0, __v: 0, company: 1, price: 1, origin: 1 }
+    { _id: 0, __v: 0, processor: 0, camera: 0, size: 0, storage: 0 }
   ).catch((err) => {
-    return res.status(404).json({ message: err.message });
+    return res.status(404).json({ error: err.message });
   });
   return res.status(200).json(mobileData);
 });
@@ -25,7 +25,7 @@ app
     // the information of mobile with name
 
     const name = req.params.name.split("-").join(" ");
-    const mobileData = await Mobile.find({ mobileName: name }, { _id: 0 });
+    const mobileData = await Mobile.find({ name: name }, { _id: 0 });
     if (!mobileData.length) {
       return res.status(404).json({ error: `${name} mobile data not found` });
     }
@@ -38,27 +38,30 @@ app
 
     if (req.headers.authorization === process.env.AUTH_ID) {
       const name = req.params.name.split("-").join(" ");
-
-      if (!req.body || !req.body.brand || !req.body.price || !req.body.origin) {
-        return res.status(204).json({ message: "invalid response" });
-      }
-
-      const mobileByName = await Mobile.find({ mobileName: name }, { _id: 0 });
+      const mobileByName = await Mobile.find({ name: name }, { _id: 0 });
       if (!mobileByName.length) {
         const mobile = new Mobile({
-          mobileName: name,
+          name: name,
           company: req.body.company,
           price: req.body.price,
           origin: req.body.origin,
+          ram: req.body.raw,
+          storage: req.body.storage,
+          processor: req.body.processor,
+          camera: req.body.camera,
+          size: req.body.size,
         });
-
-        await mobile.save();
+        await mobile.save().catch((err) => {
+          console.log(err.message);
+          return res.status(400).json({ err: err.message });
+        });
         return res.status(200).json({ message: "data added successfully" });
       } else {
         return res.status(200).json({ message: "duplicate data found" });
       }
+    } else {
+      return res.status(401).json({ error: "Invalid authorization" });
     }
-    return res.status(401).json({ error: "Invalid authorization" });
   })
   .put(async (req, res) => {
     // only authenticated user can use this method
@@ -69,7 +72,8 @@ app
       if (!req.body) {
         return res.status(204).json({ error: "Invalid response" });
       }
-      const mobile = await Mobile.find({ mobileName: name });
+
+      const mobile = await Mobile.find({ name: name });
       if (!mobile.length) {
         return res.status(404).json({ error: `${name} not found` });
       }
@@ -93,11 +97,11 @@ app
     // deleting information of mobile with name
     if (req.headers.authorization === process.env.AUTH_ID) {
       const name = req.params.name.split("-").join(" ");
-      const mobile = await Mobile.find({ mobileName: name });
+      const mobile = await Mobile.find({ nam: name });
       if (!mobile.length) {
         return res.status(404).json({ error: `${name} not found` });
       }
-      await Mobile.deleteOne({ mobileName: name }).catch((err) => {
+      await Mobile.deleteOne({ name: name }).catch((err) => {
         console.log(err);
       });
       return res
